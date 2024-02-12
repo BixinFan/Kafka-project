80d7885a170848811a289528c2f4f765c60a4cc1
BixinFan <111268635+BixinFan@users.noreply.github.com>
2024-02-11 22:51:33 -0600

    Add files via upload

---
+++ DB2.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE employees( emp_id SERIAL,
+first_name VARCHAR(100), 
+last_name VARCHAR(100), 
+dob DATE,
+city VARCHAR(100) );
---
+++ DB_1.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE employees( emp_id SERIAL,
+first_name VARCHAR(100), 
+last_name VARCHAR(100), 
+dob DATE,
+city VARCHAR(100) );
+
+CREATE TABLE employees_cdc( emp_id SERIAL,
+first_name VARCHAR(100), 
+last_name VARCHAR(100), 
+dob DATE,
+city VARCHAR(100), action varchar(100) );
